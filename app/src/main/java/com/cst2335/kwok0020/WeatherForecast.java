02ea400c21f0278c38c8a25f761638890abe4d9e
kwok0020 <kwok0020@algonquinlive.com>
2022-07-26 14:24:05 -0400

    Finished lab6_watch

--- app/src/main/java/com/cst2335/kwok0020/WeatherForecast.java
+++ app/src/main/java/com/cst2335/kwok0020/WeatherForecast.java
@@ -39,7 +39,7 @@ public class WeatherForecast extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather_forcast);
+        setContentView(R.layout.layout_notround);
 
         currentTem = findViewById(R.id.currentTem);
         max = findViewById(R.id.maxTem);
